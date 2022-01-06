9c72eb615f0752148fb5da7ac9904e8584a4f617
Marco Clarindo <marcos_clarindo@hotmail.com>
2022-01-06 19:26:50 -0300

    Atualizando o dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,70 +2,41 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import com.mysql.cj.xdevapi.Result;
 
 import db.DB;
 
-
+// Atualizando o salário de um vendedor no banco de dados
 public class Program {
 
 	public static void main(String[] args) {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		// inserindo um novo vendendor na tabela sellers
+		
 		Connection conn = null;
 		PreparedStatement st = null;
 		try {
-			// conectando ao banco de dados
 			conn = DB.getConnection();
 			
-			// inserindo dados 
 			st = conn.prepareStatement(
-					"INSERT INTO seller "
-					// campos da tabale que será preenchida
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
-			
-			//atribuindo valores em cada ?
-			st.setString(1, "Carl Gomes");
-			st.setString(2, "carl@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1987").getTime()));
-			st.setDouble(4, 4000.0);
-			st.setInt(5, 4);
-			
-			// para a chamada da alteração e saber quantas linhas foram alteradas
-			int rowsAffected = st.executeUpdate();
-			
-			if (rowsAffected > 0 ) {
-				ResultSet rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-				}
-			}
-			else {
-				System.out.println("No rows affected!");
-			}
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+"(DepartmentId = ?)");
+				//atribuindo valores ao simbolo de interrogação
+				st.setDouble(1, 200.0);
+				st.setInt(2, 2);
+				
+				// para executar o código e mostrar a quantidade de linhas afetadas
+				int rowsAffected = st.executeUpdate();
+				
+				System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
-		catch (ParseException e) {
-			e.printStackTrace();
-		}
 		finally {
 			DB.closeStatement(st);
-			// sempre fechar por último
 			DB.closeConnection();
 		}
-		
 	}
 }
