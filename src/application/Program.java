199e9e00738f248079b7bfda55a5828f70f7f152
Marco Clarindo <marcos_clarindo@hotmail.com>
2022-01-05 21:11:43 -0300

    Atualizando dados no Banco de Dados - Projeto Finalizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import com.mysql.cj.xdevapi.Result;
 
 import db.DB;
 
@@ -12,36 +17,55 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		// inserindo um novo vendendor na tabela sellers
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
-		
-		// Necessário o bloco try porque a consulta no banco de dados gera excessões
+		PreparedStatement st = null;
 		try {
-			// conectanto ao banco de dados
+			// conectando ao banco de dados
 			conn = DB.getConnection();
 			
-			// instanciando um objeto do tipo Statement
-			st = conn.createStatement();
+			// inserindo dados 
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					// campos da tabale que será preenchida
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
 			
-			// método para entrar com o comando SQL
-			rs = st.executeQuery("select * from department");
+			//atribuindo valores em cada ?
+			st.setString(1, "Carl Gomes");
+			st.setString(2, "carl@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1987").getTime()));
+			st.setDouble(4, 4000.0);
+			st.setInt(5, 4);
 			
-			// pecorrendo a tabela no banco de dados
-			while (rs.next()) {
-				System.out.println(rs.getInt("id") + ", " + rs.getString("Name"));
+			// para a chamada da alteração e saber quantas linhas foram alteradas
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0 ) {
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			}
+			else {
+				System.out.println("No rows affected!");
 			}
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
-		// fechamento do método 
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
 		finally {
-			DB.closeResultSet(rs);
 			DB.closeStatement(st);
+			// sempre fechar por último
 			DB.closeConnection();
 		}
+		
 	}
-
 }
